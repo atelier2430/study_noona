0a44668e23d9b2c091064a44ad1c9aa68249c27d
eychoi <eychoi.tinto@gmail.com>
2024-03-22 01:23:39 +0900

    [ESLINT] eslint, prettier 추가
    'ISSUE: 파일 저장 시에 세미콜론이 자동으로 붙지 않음-확인 필요'

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,9 +4,17 @@ module.exports = {
     es6: true,
     node: true,
   },
-  extends: ['airbnb'],
+  extends: ['airbnb', 'prettier'],
   rules: {
-    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
-    'react/prop-types': 'off',
+    trailingComma: 'all', // 마지막 요소 뒤에 쉼표를 항상 추가
+    arrowParens: 'always', // 화살표 함수의 매개변수가 하나인 경우에도 괄호를 항상 추가
+    singleQuote: true, // 문자열을 항상 작은따옴표로 변경
+    printWidth: 80, // 줄 바꿈을 할 때 줄 너비를 80으로 제한
+    tabWidth: 2, // 탭의 너비를 2로 설정
+    semi: true, // 항상 세미콜론을 추가
+    bracketSpacing: true, // 객체 리터럴의 괄호 안에 공백을 추가
+    jsxBracketSameLine: false, // JSX의 꺽쇠 괄호를 여는 부분을 항상 새 줄에 추가
+    jsxSingleQuote: false, // JSX 속성 값을 항상 작은따옴표로 변경
+    endOfLine: 'auto', // 줄 바꿈 문자를 자동으로 선택
   },
 };
--- README.md
+++ README.md
@@ -1,3 +1,26 @@
+# prettier
+
+trailingComma: 'all'
+: 마지막 요소 뒤에 쉼표를 항상 추가
+arrowParens: 'always'
+: 화살표 함수의 매개변수가 하나인 경우에도 괄호를 항상 추가
+singleQuote: true
+: 문자열을 항상 작은따옴표로 변경
+printWidth: 80
+: 줄 바꿈을 할 때 줄 너비를 80으로 제한
+tabWidth: 2
+: 탭의 너비를 2로 설정
+semi: true
+: 항상 세미콜론을 추가
+bracketSpacing: true
+: 객체 리터럴의 괄호 안에 공백을 추가
+jsxBracketSameLine: false
+: JSX의 꺽쇠 괄호를 여는 부분을 항상 새 줄에 추가
+jsxSingleQuote: false
+: JSX 속성 값을 항상 작은따옴표로 변경
+endOfLine: 'auto'
+: 줄 바꿈 문자를 자동으로 선택
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).

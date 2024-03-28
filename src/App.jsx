54a154b1d3923ea51353569b006f5f551a5ef68f
eychoi <atelier2430@gmail.com>
2024-03-28 22:01:32 +0900

    [MODIFY] hnm 로딩컴포넌트 추가

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ function App() {
   const location = useLocation();
   const { pathname } = location;
   const [currPathName, SetCurrPathName] = useState('')
+  const [isLogin, setIsLogin] = useState(false)
 
   useEffect(() => {
     SetCurrPathName(pathname)
@@ -25,15 +26,15 @@ function App() {
       {/* 홈을 제외하고 뒤로가기 출력 */}
       {currPathName !== "/" && <Link to="/" className="back" />}
       {/* hnm 쇼핑몰 일때만 Header 출력 */}
-      {currPathName.includes('hnm') && <Header />}
+      {currPathName.includes('hnm') && <Header isLogin={isLogin} setIsLogin={setIsLogin} />}
       <Routes>
         <Route path="/" element={<Index />} />
         <Route path="/rock-paper-scissors" element={<RockPaperScissors />} />
         <Route path="/rock-paper-scissors-class" element={<RockPaperScissorsClass />} />
         <Route path="/weather" element={<Weather />} />
         <Route path="/hnm" element={<ProductAll />} />
-        <Route path="/hnm/login" element={<Login />} />
-        <Route path="/hnm/product/:id" element={<ProductDetail />} />
+        <Route path="/hnm/login" element={<Login setIsLogin={setIsLogin} />} />
+        <Route path="/hnm/product/:id" element={<ProductDetail isLogin={isLogin}/>} />
       </Routes>
     </div>
   );
--- src/pages/shopping/ProductAll.jsx
+++ src/pages/shopping/ProductAll.jsx
@@ -1,13 +1,21 @@
 import React, { useEffect, useState } from 'react'
 import { Container, Row, Col } from 'react-bootstrap';
+import LoadingComp from '../../component/common/Loading';
 import ProductItem from '../../component/shopping/ProductItem'
 
 function ProductAll() {
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    setLoading(true)
+  },[])
+
   const API_JSON_SERVER = process.env.REACT_APP_API_JSON_SERVER
   const [productList, setProductList] = useState([])
   const getProducts = async () => {
     const url = `${API_JSON_SERVER}/products`;
     try {
+      setLoading(true)
       const res = await fetch(url, {
         headers: {
           Accept: "application/json",
@@ -17,7 +25,8 @@ function ProductAll() {
         throw new Error('Failed to fetch products');
       }
       const data = await res.json();
-      setProductList(data);
+      setLoading(false)
+      setProductList(data)
     } catch (error) {
       throw new Error(error)
     }
@@ -29,15 +38,17 @@ function ProductAll() {
 
   return (
     <div>
-      <Container className="product-list">
-        <Row>
-        {productList.length && productList.map((product) => (
-          <Col lg={3}>
-            <ProductItem product={product}/>
-          </Col>
-        ))}
-        </Row>
-      </Container>
+      {loading?
+        <LoadingComp loading={loading}/>
+        :(<Container className="product-list">
+          <Row>
+          {productList.length && productList.map((product) => (
+            <Col lg={3} key={product.id}>
+              <ProductItem product={product}/>
+            </Col>
+          ))}
+          </Row>
+        </Container>)}
     </div>
   )
 }

74a974fb72ef9e7c568ff2743df5f881bd1881cd
eychoi <atelier2430@gmail.com>
2024-03-28 22:01:03 +0900

    [MODIFY] 상품 항목 금액 세자리수 콤마 추가

--- src/component/shopping/ProductItem.jsx
+++ src/component/shopping/ProductItem.jsx
@@ -1,17 +1,34 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 function ProductItem({product}) {
+  const [price, setPrice] = useState('')
+
+  const formatNumberComma = (num) => {
+    const regexp = /\B(?=(\d{3})+(?!\d))/g;
+    setPrice(num.toString().replace(regexp, ','))
+  }
+
+  useEffect(()=>{
+    formatNumberComma(product.price)
+  },[])
+
   return (
-    <li key={product.id}>
-      <img src={product.img} alt={`product${product.id}`} />
-      <div className="title">{product.title}</div>
-      <div className="price">{product.price}</div>
-      <div className="choice">{product.choice}</div>
-      <div className="new">{product.new}</div>
-      <div className="size">
-        {product.size.map((size)=>(
-          <span>{size}</span>
-        ))}
+    <li className="product-item">
+      <div className="img">
+        <img src={product.img} alt={`product${product.id}`} />
+        <div className="size">
+          {product.size.map((size)=>(
+            <span key={size}>{size}</span>
+            ))}
+        </div>
+      </div>
+      <div className="product-info">
+        <div className="title">
+          {product.title}
+          {product.choice?(<span className="tag pick">PICK</span>):null}
+          {product.new?(<span className="tag new">NEW</span>):null}
+        </div>
+        <div className="price">&#8361; {price}</div>
       </div>
     </li>
   )

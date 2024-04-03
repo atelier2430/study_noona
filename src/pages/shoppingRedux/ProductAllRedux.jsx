7f8c53e1b6faefcc1431e2ff929f027eb60b6c08
eychoi <eychoi.tinto@gmail.com>
2024-04-04 01:28:02 +0900

    [UPDATE] createAsyncThunk 적용
    'hnm 상품디테일 포함'

--- src/pages/shoppingRedux/ProductAllRedux.jsx
+++ src/pages/shoppingRedux/ProductAllRedux.jsx
@@ -2,25 +2,29 @@ import React, { useEffect, useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import { Container, Row, Col } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
+import { fetchProducts } from '../../redux/reducer/productReducer';
 import ProductItemRedux from '../../component/shoppingRedux/ProductItem';
-import productAction from '../../redux/action/productAction'
+import LoadingComp from '../../component/common/Loading';
 
 function ProductAllRedux() {
-  const dispatch = useDispatch()
-  const [query, setQuery] = useSearchParams();
-  const [viewColumn, setViewColumn] = useState(4);
-
   const productList = useSelector(state=>state.product.productList)
+  const isLoading = useSelector(state=>state.product.isLoading)
+  const [query, setQuery] = useSearchParams();
+  const dispatch = useDispatch()
+  const [viewColumn, setViewColumn] = useState(4);
 
   const getProducts = () => {
     const searchQuery = query.get('q');
-    dispatch(productAction.getProducts(searchQuery))
+    dispatch(fetchProducts(searchQuery))
   };
 
   useEffect(() => {
-    getProducts();
     setQuery()
   }, [query, viewColumn]);
+
+  useEffect(() => {
+    getProducts();
+  },[])
 
   const sortList = [
     { id: 'orderReset', label: '추천순', defaultChecked: true, sort: '', order: '' },
@@ -36,33 +40,40 @@ function ProductAllRedux() {
   return (
     <div>
       <Container className="product-list">
-        <div className="order-area">
-          {sortList.map((sort) => (
-            <label key={sort.id} htmlFor={sort.id} className="radio-area">
-              <input
-                type="radio"
-                name="productSort"
-                id={sort.id}
-                defaultChecked={sort.defaultChecked}
-              />
-              <span>{sort.label}</span>
-            </label>
-          ))}
-          <button type="button" className="icon view-big" onClick={() => clickViewColumn(4)}>
-            크게 보기
-          </button>
-          <button type="button" className="icon view-small" onClick={() => clickViewColumn(2)}>
-            작게 보기
-          </button>
-        </div>
-        <Row>
-          {productList.length > 0 &&
-            productList.map((product) => (
+        {isLoading ? (
+          <LoadingComp loading={isLoading}/>
+        ):(
+          <>
+          <div className="order-area">
+            {sortList.map((sort) => (
+              <label key={sort.id} htmlFor={sort.id} className="radio-area">
+                <input
+                  type="radio"
+                  name="productSort"
+                  id={sort.id}
+                  defaultChecked={sort.defaultChecked}
+                />
+                <span>{sort.label}</span>
+              </label>
+            ))}
+            <button type="button" className="icon view-big" onClick={() => clickViewColumn(4)}>
+              크게 보기
+            </button>
+            <button type="button" className="icon view-small" onClick={() => clickViewColumn(2)}>
+              작게 보기
+            </button>
+          </div>
+          <Row>
+            {productList && productList.length > 0 ? (
+              productList.map((product) => (
               <Col lg={viewColumn} key={product.id}>
                 <ProductItemRedux product={product} />
               </Col>
-            ))}
-        </Row>
+              ))
+            ) : <p>상품이 없습니다.</p>}
+          </Row>
+          </>
+        )}
       </Container>
     </div>
   );
--- src/redux/reducer/productReducer.jsx
+++ src/redux/reducer/productReducer.jsx
@@ -1,24 +1,77 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 
-const initialState={
+const initialState = {
   productList: [],
-  productDetail: null
-}
+  productDetail: null,
+  isLoading: false,
+};
+const API_JSON_SERVER = process.env.REACT_APP_API_JSON_SERVER;
+
+export const fetchProducts = createAsyncThunk('product/fetchAll', async (searchQuery) => {
+  try{
+    const url = `${API_JSON_SERVER}/products?q=${searchQuery || ''}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    return data;
+  }catch(error){
+    throw new Error(error.message);
+  }
+});
+
+export const fetchSingleProduct = createAsyncThunk('product/fetchSingle', async (id) => {
+  try{
+    const url = `${API_JSON_SERVER}/products/${id}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    return data;
+  }catch(error){
+    throw new Error(error.message);
+  }
+});
 
 const productSlice = createSlice({
   name: 'product',
   initialState,
   reducers: {
-    getAllProducts(state, action) {
+    getSingleProduct(state, action) {
       // eslint-disable-next-line no-param-reassign
-      state.productList = action.payload.productList
-    }, 
-    getSingleProduct(state,action){
-      // eslint-disable-next-line no-param-reassign
-      state.productDetail = action.payload.productDetail
-    }
-  }
-})
+      state.productDetail = action.payload.productDetail;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchProducts.pending, (state) => {
+        // eslint-disable-next-line no-param-reassign
+        state.isLoading = true;
+      })
+      .addCase(fetchProducts.fulfilled, (state, action) => {
+        // eslint-disable-next-line no-param-reassign
+        state.isLoading = false;
+        // eslint-disable-next-line no-param-reassign
+        state.productList = action.payload;
+        // eslint-disable-next-line no-param-reassign
+        state.productDetail = null;
+      })
+      .addCase(fetchProducts.rejected, (state) => {
+        // eslint-disable-next-line no-param-reassign
+        state.isLoading = false;
+      })
+      .addCase(fetchSingleProduct.pending, (state) => {
+        // eslint-disable-next-line no-param-reassign
+        state.isLoading = true;
+      })
+      .addCase(fetchSingleProduct.fulfilled, (state, action) => {
+        // eslint-disable-next-line no-param-reassign
+        state.isLoading = false;
+        // eslint-disable-next-line no-param-reassign
+        state.productDetail = action.payload;
+      })
+      .addCase(fetchSingleProduct.rejected, (state) => {
+        // eslint-disable-next-line no-param-reassign
+        state.isLoading = false;
+      });
+  },
+});
 
-export const productActions = productSlice.actions
-export default productSlice.reducer
+export const productActions = productSlice.actions;
+export default productSlice.reducer;

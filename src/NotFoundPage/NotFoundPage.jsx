8706d169d610965e08183ca3f56b0f6e77e46bc0
eychoi <eychoi.tinto@gmail.com>
2024-04-11 02:01:59 +0900

    [ADD] 넷플릭스 Not Found Page 뒤로가기 추가

--- src/NotFoundPage/NotFoundPage.jsx
+++ src/NotFoundPage/NotFoundPage.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 import backgroundImage404 from '../assets/images/project05_movie/404.png'
 
 function NotFoundPage() {
+  const navigate = useNavigate()
   return (
     <div className="page-404">
         <img src={backgroundImage404} alt="404 page"/>
+        <button type="button" className="history-back" onClick={() => navigate(-1)}>돌아가기</button>
     </div>
   )
 }
